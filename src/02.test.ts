b1c93cd21c5866518ebbb88780a25d66edf0c873
Maciej Ziehlke <mac@dmio.co>
2023-12-04 23:10:23 +0000

    Move test data to avoid dups

--- src/02.test.ts
+++ src/02.test.ts
@@ -1,5 +1,11 @@
 import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
 
+const testData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
+
 type Cubes = {
   red: number;
   green: number;
@@ -25,12 +31,6 @@ const games1 = (data: string, config: Cubes) =>
     .reduce((acc, el, i) => acc + (el ? i + 1 : 0), 0);
 
 Deno.test("Advent of code, day 2", () => {
-  const testData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
-
   const config = {
     red: 12,
     green: 13,
@@ -65,11 +65,5 @@ const games2 = (data: string) => {
 };
 
 Deno.test("Advent of code, day 2, v2", () => {
-  const testData = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-  Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-  Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-  Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
-
   assertEquals(games2(testData), 2286);
 });
