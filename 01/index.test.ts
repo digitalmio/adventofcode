e7a7aa3af21fa504fc9f9c73a9889ea57743be1a
Maciej Ziehlke <mac@dmio.co>
2023-12-04 14:50:51 +0000

    Day 1, v2

--- 01/index.test.ts
+++ 01/index.test.ts
@@ -5,12 +5,48 @@ const calculate = (data: string): number => data.split('\n').reduce((acc, el) =>
   return acc + Number(numbers.at(0)!  + numbers.at(-1)!);
 }, 0);
 
-Deno.test('Advent of code day 1', () => {
-  const testData = 
-`1abc2
+Deno.test("Advent of code day 1", () => {
+  const testData = `1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`;
 
   assertEquals(calculate(testData), 142);
+});
+
+const calculateV2 = (data: string): number =>
+  data.split("\n").reduce((acc, el) => {
+    const numberToDigits = {
+      one: "o1ne",
+      two: "t2wo",
+      three: "t3hree",
+      four: "f4our",
+      five: "f5ive",
+      six: "s6ix",
+      seven: "s7even",
+      eight: "e8ight",
+      nine: "n9ine",
+    };
+
+    const finalNums = Object.entries(numberToDigits).reduce(
+      (acc, [str, num]) => {
+        return acc.replaceAll(str, num);
+      },
+      el
+    );
+
+    const numbers = finalNums.replaceAll(/[^0-9]/g, "");
+    return acc + Number(numbers.at(0)! + numbers.at(-1)!);
+  }, 0);
+
+Deno.test("Advent of code day 1-v2", () => {
+  const testData = `two1nine
+  eightwothree
+  abcone2threexyz
+  xtwone3four
+  4nineeightseven2
+  zoneight234
+  7pqrstsixteen`;
+
+  assertEquals(calculateV2(testData), 281);
 });
