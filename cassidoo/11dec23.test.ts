0fa98d5ef94e62b3bd54001724d762b56c0f5950
Maciej Ziehlke <mac@dmio.co>
2023-12-13 00:35:23 +0000

    Cassidoo 11-12-23

---
+++ cassidoo/11dec23.test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { majority } from "./11dec23.ts";
+
+Deno.test("Should return set number", () => {
+  assertEquals(majority([3, 1, 4, 1]), 1);
+});
+
+Deno.test("Should return odd majority", () => {
+  assertEquals(majority([33, 44, 55, 66, 77]), "odd");
+});
+
+Deno.test("Should return odd majority", () => {
+  assertEquals(majority([22, 33, 44, 55, 66, 77, 88]), "even");
+});
+
+Deno.test("Should return no majority", () => {
+  assertEquals(majority([1, 2, 3, 4]), "no majority");
+});
