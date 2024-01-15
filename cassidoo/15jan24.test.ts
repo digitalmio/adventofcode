923f89794ef5e8e6741a41b4c20c6c48636bdc16
Maciej Ziehlke <mac@dmio.co>
2024-01-15 13:03:52 +0000

    Cassidoo 15Jan24

---
+++ cassidoo/15jan24.test.ts
@@ -0,0 +1,24 @@
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { flip } from "./15jan24.ts";
+
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+Deno.test("Should return set for horizontal", () => {
+  assertEquals(flip(array, "horizontal"), [
+    [3, 2, 1],
+    [6, 5, 4],
+    [9, 8, 7],
+  ]);
+});
+
+Deno.test("Should return set for vertical", () => {
+  assertEquals(flip(array, "vertical"), [
+    [7, 8, 9],
+    [4, 5, 6],
+    [1, 2, 3],
+  ]);
+});
---
+++ cassidoo/15jan24.ts
@@ -0,0 +1,7 @@
+export const flip = (
+  arr: number[][],
+  type: "horizontal" | "vertical"
+): number[][] => {
+  if (type === "vertical") return arr.toReversed();
+  return arr.map((el) => el.toReversed());
+};
