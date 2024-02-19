03acc61b215e30bf54af8329980a7cfc53a543d0
Maciej Ziehlke <mac@dmio.co>
2024-02-19 15:12:16 +0000

    Join filters into 1

--- cassidoo/19feb24.ts
+++ cassidoo/19feb24.ts
@@ -2,8 +2,10 @@ export const wordLengthProduct = (input: string[]): number =>
   input
     .map((inputEl, i) => {
       const unique = input
-        .filter((_, ii) => i !== ii)
-        .filter((el) => inputEl.split("").every((val) => !el.includes(val)))
+        .filter(
+          (el, ii) =>
+            i !== ii && inputEl.split("").every((val) => !el.includes(val))
+        )
         .sort((a, b) => b.length - a.length);
 
       return unique.length ? unique[0].length * inputEl.length : 0;
@@ -12,7 +14,7 @@ export const wordLengthProduct = (input: string[]): number =>
     .at(-1) || 0;
 
 console.log(
-  wordLengthProduct(["fish", "fear", "boo", "egg", "cake", "abcdef"])
+  wordLengthProduct(["fish", "fear", "boo", "egg", "cake", "abcdef"]) // 16
 );
 
-console.log(wordLengthProduct(["a", "aa", "aaa", "aaaa"]));
+console.log(wordLengthProduct(["a", "aa", "aaa", "aaaa"])); // 0
