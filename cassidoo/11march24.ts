6af646e707dd5114f503e641a2c84a69e5d614a4
Maciej Ziehlke <mac@dmio.co>
2024-03-12 13:27:26 +0000

    Reduce version

--- cassidoo/11march24.ts
+++ cassidoo/11march24.ts
@@ -7,4 +7,16 @@ export const maxGap = (userArr: number[]): number =>
       .filter(Boolean)
   );
 
+export const maxGapReduce = (userArr: number[]): number =>
+  userArr.sort().reduce((acc, el, i, arr) => {
+    const calc = el - arr[i - 1];
+    return !isNaN(calc) && calc > acc ? calc : acc;
+  }, 0);
+
 console.log(maxGap([3, 6, 9, 1, 2])); // 3
+console.log(maxGap([])); // 0
+console.log(maxGap([1])); // 0
+
+console.log(maxGapReduce([3, 6, 9, 1, 2])); // 3
+console.log(maxGapReduce([])); // 0
+console.log(maxGapReduce([1])); // 0
