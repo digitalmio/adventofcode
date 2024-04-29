bf18451eb957a6658005fdf4799f906b39708f45
Maciej Ziehlke <mac@dmio.co>
2024-04-29 12:10:49 +0100

    29 april

---
+++ cassidoo/29apr24.ts
@@ -0,0 +1,12 @@
+const chars = "qwertyuiop[asdfghjkl;zxcvbnm,".split("");
+
+const translateRightShift = (str: string) =>
+  str
+    .split("")
+    .map((el) =>
+      el !== " " ? chars.at(chars.findIndex((chEl) => chEl === el) - 1) : " "
+    )
+    .join("");
+
+console.log(translateRightShift(";p; epeor"));
+console.log(translateRightShift("ejp s, o"));
