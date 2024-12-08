c788dfafbeba535c274cf93251c4606aed175cc5
Maciej Ziehlke <mac@dmio.co>
2024-12-08 19:51:37 +0000

    Day 8, part 1 only

--- 2024/07.ts
+++ 2024/07.ts
@@ -9,57 +9,57 @@ const input = `190: 10 19
 292: 11 6 16 20`;
 
 const inputArr = input.split("\n").map((el) => {
-  const temp = el.split(":");
-  return {
-    result: Number.parseInt(temp[0]),
-    numbers: temp[1].trim().split(" ").map(Number),
-  };
+	const temp = el.split(":");
+	return {
+		result: Number.parseInt(temp[0]),
+		numbers: temp[1].trim().split(" ").map(Number),
+	};
 });
 
 function checkRow({
-  result,
-  numbers,
+	result,
+	numbers,
 }: { result: number; numbers: number[] }): boolean {
-  function tryMaths(currentValue: number, restNums: number[]): boolean {
-    if (!restNums || restNums.length === 0) {
-      return currentValue === result;
-    }
+	function tryMaths(currentValue: number, restNums: number[]): boolean {
+		if (!restNums || restNums.length === 0) {
+			return currentValue === result;
+		}
 
-    const [num, ...rest] = restNums;
+		const [num, ...rest] = restNums;
 
-    // add +
-    if (currentValue + num <= result && tryMaths(currentValue + num, rest)) {
-      return true;
-    }
+		// add +
+		if (currentValue + num <= result && tryMaths(currentValue + num, rest)) {
+			return true;
+		}
 
-    // multiply *
-    if (currentValue * num <= result && tryMaths(currentValue * num, rest)) {
-      return true;
-    }
+		// multiply *
+		if (currentValue * num <= result && tryMaths(currentValue * num, rest)) {
+			return true;
+		}
 
-    // concatenate ||
-    const concatenated = Number.parseInt(`${currentValue}${num}`);
-    if (concatenated <= result && tryMaths(concatenated, rest)) {
-      return true;
-    }
+		// concatenate ||
+		const concatenated = Number.parseInt(`${currentValue}${num}`);
+		if (concatenated <= result && tryMaths(concatenated, rest)) {
+			return true;
+		}
 
-    return false;
-  }
+		return false;
+	}
 
-  const [firstNum, ...restNums] = numbers;
-  return tryMaths(firstNum, restNums);
+	const [firstNum, ...restNums] = numbers;
+	return tryMaths(firstNum, restNums);
 }
 
 // get sum
 console.time();
 const resultSum = inputArr.reduce(
-  (acc, el) => acc + (checkRow(el) ? el.result : 0),
-  0,
+	(acc, el) => acc + (checkRow(el) ? el.result : 0),
+	0,
 );
 console.timeEnd();
 
 console.log({
-  resultSum,
+	resultSum,
 });
 
-export { };
+export {};
---
+++ 2024/08.ts
@@ -0,0 +1,75 @@
+const input = `............
+........0...
+.....0......
+.......0....
+....0.......
+......A.....
+............
+............
+........A...
+.........A..
+............
+............`;
+
+// this is part 1 only
+
+const isInRange = (start: number, end: number, check: number) =>
+  Boolean(check <= end && check >= start);
+
+const findAntinodes = (input: string) => {
+  const grid = input.split("\n").map((line) => line.split(""));
+  const rows = grid.length - 1; // need this zero-indexed
+  const cols = grid[0].length - 1; // same zero-index
+
+  const generateAntinodePos = (
+    pos: [number, number],
+    diff: [number, number],
+  ) => {
+    const antinodeY = pos[0] + diff[0];
+    const antinodeX = pos[1] + diff[1];
+
+    return isInRange(0, cols, antinodeX) && isInRange(0, rows, antinodeY) ?
+      `${antinodeY},${antinodeX}` : null;
+  };
+
+  // Find all antennas, returns row, col
+  const antennas = grid.reduce(
+    (acc, row, rowI) => {
+      row.forEach((el, i) => {
+        if (el !== ".") {
+          acc[el] = [...(acc[el] || []), [rowI, i]];
+        }
+      });
+      return acc;
+    },
+    {} as Record<string, Array<[number, number]>>,
+  );
+
+  const antinodes = [
+    ...new Set(
+      Object.keys(antennas)
+        .flatMap((antennaGroupName) => {
+          return antennas[antennaGroupName].flatMap((antenna, i, groupArr) => {
+            return groupArr.flatMap((a, ai) => {
+              if (i === ai) {
+                return null;
+              }
+
+              const diffY = antenna[0] - a[0]; // rows
+              const diffX = antenna[1] - a[1]; // cols
+
+              return generateAntinodePos(antenna, [diffY, diffX]);
+            });
+          });
+        })
+        .filter(Boolean),
+    ),
+  ];
+
+  return {
+    antinodes,
+    antinodesCount: antinodes.length,
+  };
+};
+
+console.log(findAntinodes(input));
